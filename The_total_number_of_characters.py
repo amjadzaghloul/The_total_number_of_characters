dbc11e56f8dc53df565b4b2c7540ab2e1d4b279d
Amjad <amjad.zaghloul@gmail.com>
2018-03-13 19:37:52 +0300

    A function has been created to determine the number of characters in the sentence

---
+++ The_total_number_of_characters.py
@@ -0,0 +1,4 @@
+def the_total_number_of_characters():
+	sentence = raw_input("Please enter the sentence")
+	return "The number of characters for this sentence " + str(len(sentence)) + " Letter"
+print the_total_number_of_characters()
